2f92c3879bcefb2f3626528366cb66066e1d4ebd
Raimond Lume <lume.raimond@gmail.com>
2019-09-02 09:24:30 +0300

    fix typo in footer

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -3,7 +3,7 @@
     <div class="content has-text-centered">
       <p>
         <strong>fuud?</strong> by <a href="https://raimondlu.me" target="_blank" rel="noopener noreferrer">raimondlu.me</a><br>
-        source at <a href="https://github.com/raimondlume/fuud-clinet">github</a>
+        source at <a href="https://github.com/raimondlume/fuud-client">github</a>
       </p>
     </div>
   </footer>
